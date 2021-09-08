d2fcbef2d870d36bdd5ae75fb70c8fc7b1b51c79
Dinithi Hasanika <dinithiliyanage.95@gmail.com>
2021-09-08 18:16:54 +0530

    cors

--- src/main/java/ecube/controller/UserController.java
+++ src/main/java/ecube/controller/UserController.java
@@ -3,6 +3,7 @@ package ecube.controller;
 import ecube.Service.UserService;
 import ecube.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class UserController
     @Autowired
     private UserService userService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/users")
     List<User> getUsers(){
         return userService.getUsers();
